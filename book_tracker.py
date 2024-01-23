2577c86f5f4bdf14d7d0e0dd47783d1a26cbfa9a
Ugyen Norbu <unorbu@moit.gov.bt>
2024-01-23 20:35:55 +0600

    Feat: ReadingList modified
    - added "availability" in the db table
    - added docstring
    - deleted unwanted codes

--- book_tracker.py
+++ book_tracker.py
@@ -41,38 +41,41 @@ class Book():
         reading_list._update_book_status(self)
         print(f"- Status of '{self.name}' updated to 'Completed'.")
 class ReadingList:
+    """
+    A class to represent a reading list.
+    """
 
     def __init__(self):
-        self.books = []
+        """
+        Initializes a reading list object.
+        """
         self.conn = sqlite3.connect("library.db")
         self.cursor = self.conn.cursor()
-        self.cursor.execute(""" 
+        self.cursor.execute(
+            """ 
             CREATE TABLE IF NOT EXISTS reading_list(
                 name TEXT,
                 author TEXT,
-                status TEXT
+                status TEXT,
+                availability TEXT
             )
         """)
         print(f"* Reading list created.\n")
         
     def add_book(self, book):
+        """
+        Adds a book to the reading list.
+
+        This method adds a book to the library's list of books. It checks if the book is already in the library before adding it.
+
+        Args:
+            book (Book): A book object which contains the details of the book.
+        """
         all_books = self.cursor.execute("SELECT * FROM reading_list WHERE name = ? AND author = ?", (book.name, book.author))
         if not all_books.fetchone():
-            self.cursor.execute("INSERT INTO reading_list VALUES (?, ?, ?)", (book.name, book.author, book.status))
+            self.cursor.execute("INSERT INTO reading_list VALUES (?, ?, ?, ?)", (book.name, book.author, book.status, book.availability))
             self.conn.commit()
             print(f"- '{book.name}' added to the reading list.")
         else:
             print(f"- ERROR: '{book.name}' cannot be added to the reading list as it already exists.")
     
-    # def display_reading_list(self):
-    #     for book in self.books:
-    #         print(f"Title:      {book.name}")
-    #         print(f"Author:     {book.author}")
-    #         print(f"Status:     {book.status}")
-    #         if book.start:
-    #             start_date = book.start.strftime("%A, %-dth %b. %Y")
-    #             print(f"Start:      {start_date}")
-            
-    #         if book.end:
-    #             end_date = book.end.strftime("%A, %-dth %b. %Y")
-    #             print(f"End:        {end_date}")
